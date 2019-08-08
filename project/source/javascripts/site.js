6a6b67ed3d51445f9a6c3b74bd4e4c75b5c4bb0e
catalina baron <catalinatbaron@gmail.com>
2019-08-08 12:46:19 +0300

    use of the grid, changes in sizes small animation on scroll

--- project/source/javascripts/site.js
+++ project/source/javascripts/site.js
@@ -2,7 +2,8 @@
 const svgTriangle = document.querySelector(".svg__triangle");
 const body = document.querySelector("body");
 
-svgTriangle.addEventListener("click", () => {
+svgTriangle.addEventListener("click", (event) => {
+  event.preventDefault();
   svgTriangle.classList.toggle("svg__triangle-onClick");
   // const body = document.querySelector("body");
   body.firstElementChild.classList.toggle("dropdown-show");
@@ -32,3 +33,38 @@ about.addEventListener("click", function(event) {
   svgTriangle.classList.toggle("svg__triangle-onClick");
   body.firstElementChild.classList.toggle("dropdown-show");
 })
+
+
+const controller = new ScrollMagic.Controller();
+
+const scene =  new ScrollMagic.Scene({
+  triggerElement: '.services__heading'
+})
+.setClassToggle('.cards', 'show')
+.addTo(controller);
+
+
+const controller2 = new ScrollMagic.Controller();
+
+const scene2 =  new ScrollMagic.Scene({
+  triggerElement: '.about__heading'
+})
+.setClassToggle('.card-team', 'show')
+.addTo(controller2);
+
+
+const controller3 = new ScrollMagic.Controller();
+
+const scene3 =  new ScrollMagic.Scene({
+  triggerElement: '.clients__heading'
+})
+.setClassToggle('.clients', 'show')
+.addTo(controller2);
+
+const controller4 = new ScrollMagic.Controller();
+
+const scene4 =  new ScrollMagic.Scene({
+  triggerElement: '.about'
+})
+.setClassToggle('.about__text', 'show')
+.addTo(controller4);
